fbe19e3e8e8fb3fa42e0658eceee32dc254f533d
Kevi-sito <deleon.martinez.24079@itsmante.edu.mx>
2026-02-23 22:58:39 -0600

    Se agrega cambios

--- src/code/GUI.java
+++ src/code/GUI.java
@@ -2,18 +2,28 @@ package code;
 import javax.swing.*;
 import java.awt.*;
 public class GUI extends JFrame {
+    public JPanel mainPanel;
+    public CardLayout cardLayout;
     public GUI() {
         configFrame();
+        initComponents();
     }
     
     private void configFrame() {
         setSize(800, 600);
-        setLocationRelativeTo(null);
-        
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("BatiPlayer");
+        setLocationRelativeTo(null);     
+        setDefaultCloseOperation(EXIT_ON_CLOSE);         
     }
     
     private void initComponents() {
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        getContentPane().add(mainPanel);
+        
+    }
+    
+    private void menu() {
         
     }
 }
